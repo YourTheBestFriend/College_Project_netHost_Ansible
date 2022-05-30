03c77feb335d35433dc5832824ab936c692d6cd8
YourTheBestFriend <goncharovgithub8@gmail.com>
2022-05-30 22:54:09 +0000

    update code and try set info in file *xlsx

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
+from concurrent.futures import thread
+# import multiprocessing
 from sys import *
+from threading import Thread
 from clr import *
 from PySide6.QtWidgets import* # QApplication, QMainWindow
 from PySide6.QtCore import* # QFile
 from netHost import *
 AddReference('DLL/ansible_host') # Include Lib
 from CSharpAnsible import AnsibleHost # Import C# DLL
-from multiprocessing import Process
+# import threading
+import time
+import openpyxl # Create and write doc *xlsx
+# def functionMultiProcessing(function):
+#     p = multiprocessing.Process(target=function)
+#     p.start()
+#     # p.join()
+#     return p.communicate()
 
 class MainWindow(QMainWindow):
     # create empty host
     host = AnsibleHost('none', 'none','none') # 'none' 
+    path_table = 'table_systeminfo.xlsx'
     def __init__(self):
        # For driwing
        super(MainWindow, self).__init__()
@@ -27,7 +38,7 @@ class MainWindow(QMainWindow):
 
     def ping_host(self):
         try:
-            self.ui.textEdit.setText(self.host.PingHost(self.ui.lineEdit_5.text()))
+            self.ui.textEdit.setText(self.host.PingHost(self.ui.lineEdit_5.text())) # functionMultiProcessing()
             self.ui.textEdit_2.setText('ping_host Complete')
         except:
             self.ui.textEdit_2.setText('Error ping_host')
@@ -35,20 +46,49 @@ class MainWindow(QMainWindow):
     def get_systeminfo(self):
         try:
             # print(self.host.PrintSystemInfo(self.ui.lineEdit_5.text()))
-            self.ui.textEdit.setText(self.host.PrintSystemInfo(self.ui.lineEdit_5.text()))
+            value_ = self.host.PrintSystemInfo(self.ui.lineEdit_5.text())
+            self.ui.textEdit.setText(value_)
+            # string.encode(encoding='UTF-8',errors='strict')
+            value_.encode(encoding='OEM',errors='strict')
+            value_ = value_.split(':')
             
+            wb = openpyxl.Workbook() 
+            sheet = wb.active 
+            
+            row_ = 2
+            col_ = 2
+            flag = True
+
+            for i in value_:
+                c = sheet.cell(row = row_, column = col_)
+                c.value = i.replace(' ', '')
+                if flag == True:
+                    col_ += 1
+                    flag = False
+                else:
+                    col_ -= 1
+                    flag = True
+                if i == 'WORKGROUP':
+                    break
+            wb.save("sample.xlsx") 
+
             self.ui.textEdit_2.setText('get_systeminfo')
         except:
             self.ui.textEdit_2.setText('Error get_systeminfo')
     
     def get_program(self):
         try:
-            # print(self.host.PrintSystemInfo(self.ui.lineEdit_5.text()))
             self.ui.textEdit.setText(self.host.PrintProgram(self.ui.lineEdit_5.text()))
-            
             self.ui.textEdit_2.setText('get_program')
         except:
             self.ui.textEdit_2.setText('Error get_program')
+    
+    def get_use_whatever(self):
+        try:
+            self.ui.textEdit.setText(self.host.MyCommand(self.ui.lineEdit_6.text()))
+            self.ui.textEdit_2.setText('get_use_whatever')
+        except:
+            self.ui.textEdit_2.setText('Error get_use_whatever')
 
 # Create Application 
 app = QApplication(argv)
@@ -62,7 +102,9 @@ x_window.ui.pushButton_2.clicked.connect(x_window.ping_host)
 # info 
 x_window.ui.pushButton_3.clicked.connect(x_window.get_systeminfo)
 # program
-x_window.ui.pushButton_4.clicked.connect(x_window.get_program)
+x_window.ui.pushButton_5.clicked.connect(x_window.get_program)
+# use your personal command (try...)
+x_window.ui.pushButton_4.clicked.connect(x_window.get_use_whatever)
 
 
 # print all hosts 
